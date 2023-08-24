e8ce2d7c8f2e1b353fb5da7b209c1537229f29af
guolam <guolam123@gmail.com>
2023-08-24 17:48:03 +0900

    question seed changed

--- onlinetool/database/migrations/2023_07_09_022303_create_questions_table.php
+++ onlinetool/database/migrations/2023_07_09_022303_create_questions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
       $table->id();
       $table->string('content');
       $table->foreignId('attribution_id')->constrained()->cascadeOnDelete();
+      $table->boolean('reversedIndices')->default(false);
       $table->timestamps();
     });
   }
