23a0b3358ae8f74aefd1a305d28933cd153b9f06
guolam <guolam123@gmail.com>
2023-08-29 13:14:14 +0900

    front page change

--- onlinetool/resources/views/welcome.blade.php
+++ onlinetool/resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css" />
     <title>株式会社OnLine | アセスメントツール</title>
+    <link rel="icon" href="/logo/logo.svg" type="image/png">
 </head>
 
 <body class="text-gray-800 antialiased">
@@ -69,8 +70,13 @@
                             <p class="mt-4 text-lg text-gray-300">
                                 ご自身のビジネス力を可視化し、
                                 どれくらいのビジネス力があるのかを知ることができます。
-
                             </p>
+                        </div>
+                        <div class="mt-8">
+                            <a href="{{ route('login') }}"
+                                class="bg-blue-500 hover:bg-blue-900 text-white font-bold py-5 px-5 rounded">
+                                無料　15分で簡単診断
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -94,10 +100,9 @@
                                     class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
                                     <i class="fas fa-award"></i>
                                 </div>
-                                <h6 class="text-xl font-semibold">Awarded Agency</h6>
+                                <h6 class="text-xl font-semibold">適切なキャリア選択のサポート</h6>
                                 <p class="mt-2 mb-4 text-gray-600">
-                                    Divide details about your product or agency work into parts.
-                                    A paragraph describing a feature will be enough.
+                                    自己理解が深まることで、個人のスキル、興味、価値観に合ったキャリアパスを選択する際により良い判断が可能となります。ツールの結果を通じて、ビジネスパーソンは自身の強みや弱みを把握し、それに基づいて将来の職業的な方向性を見極めることができるでしょう。これによって、適切な職種や業界を選択する際の迷いや失敗を減少させる助けとなります。
                                 </p>
                             </div>
                         </div>
@@ -110,10 +115,9 @@
                                     class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
                                     <i class="fas fa-retweet"></i>
                                 </div>
-                                <h6 class="text-xl font-semibold">Free Revisions</h6>
+                                <h6 class="text-xl font-semibold">効果的なコミュニケーションと協力関係の構築</h6>
                                 <p class="mt-2 mb-4 text-gray-600">
-                                    Keep you user engaged by providing meaningful information.
-                                    Remember that by this time, the user is curious.
+                                    自己理解が進むと、他人とのコミュニケーションにおいても大きな優位性を持つことができます。ビジネスパーソンが自分のコミュニケーションスタイル、意思決定方法、ストレスへの対処法を理解すれば、同僚やクライアントとの対話が円滑に進み、誤解や摩擦を減少させることができます。また、自己理解を深めることで、チーム内での協力関係の構築やリーダーシップスキルの向上にも寄与します。
                                 </p>
                             </div>
                         </div>
@@ -126,10 +130,9 @@
                                     class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
                                     <i class="fas fa-fingerprint"></i>
                                 </div>
-                                <h6 class="text-xl font-semibold">Verified Company</h6>
+                                <h6 class="text-xl font-semibold">個人の成長と能力向上の促進</h6>
                                 <p class="mt-2 mb-4 text-gray-600">
-                                    Write a few lines about each one. A paragraph describing a
-                                    feature will be enough. Keep you user engaged!
+                                    自己理解を深めるプロセスは、個人の成長と能力向上を助ける重要なステップです。ビジネスパーソンが自分の強みや成長の余地を把握すれば、継続的な学習やスキルの向上に焦点を当てることができます。自己理解は、目標設定や自己啓発の方針を効果的に導く手助けとなり、結果として個人の職業的な成功に寄与します。
                                 </p>
                             </div>
                         </div>
@@ -142,25 +145,29 @@
                             <i class="fas fa-user-friends text-xl"></i>
                         </div>
                         <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                            Working with us is a pleasure
+                            診断ツールだからこそできること
                         </h3>
                         <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                            Don't let your uses guess by attaching tooltips and popoves to
-                            any element. Just make sure you enable them first via
-                            JavaScript.
+                            <strong class="font-bold">自己認識の向上:</strong><br>
+                            診断ツールは個人に対して客観的な情報を提供することができます。自分自身のスキル、特性、価値観などについて深く理解することで、自己認識が向上します。これにより、自分の強みと弱みを把握し、自身のキャリア目標に合わせて成長戦略を立てることができます。
                         </p>
                         <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
-                            The kit comes with three pre-built pages to help you get started
-                            faster. You can change the text and images and you're good to
-                            go. Just make sure you enable them first via JavaScript.
+                            <strong class="font-bold">効果的なパーソナルブランディング:</strong><br>
+                            ビジネス環境では、個人のブランドイメージが重要です。診断ツールの結果を活用することで、自身の強みや特徴を明確にし、他人とのコミュニケーションやキャリア展望を効果的に伝えることができます。これにより、自己アピールが向上し、プロフェッショナルな印象を残すことができます。
                         </p>
-                        <a href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-                            class="font-bold text-gray-800 mt-8">Check Tailwind Starter Kit!</a>
+                        <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
+                            <strong class="font-bold">適切な成長戦略の策定:</strong><br>
+                            診断ツールによって得られるデータや洞察は、個人の成長戦略の策定に役立ちます。自身のスキルや特性の分析に基づいて、どの分野でスキルを向上させるべきか、どのようなトレーニングや学習が適しているかを把握できます。これにより、効果的なスキルアッププランを立て、職業的な成長を促進することが可能です。
+
+                            これらのポイントを活かして、ビジネスパーソン向けの診断ツールを開発し、個人の自己理解とキャリアの向上に貢献することができるでしょう。
+                        </p>
+                        <a href="{{ route('login') }}" class="font-bold text-gray-800 mt-8">今すぐ診断&nbsp;<iconify-icon
+                                icon="uil:angle-right-b" style="color: #222;"></iconify-icon></a>
                     </div>
                     <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
                         <div
                             class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-600">
-                            <img alt="hero" src="{{ asset('img/hero.jpg') }}"
+                            <img alt="hero" src="{{ asset('/assets/img/tool.jpg') }}"
                                 class="w-full align-middle rounded-t-lg" />
                             <blockquote class="relative p-8 mb-4">
                                 <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
@@ -170,12 +177,12 @@
                                     </polygon>
                                 </svg>
                                 <h4 class="text-xl font-bold text-white">
-                                    Top Notch Services
+                                    自己認識の進化をサポートする診断ツール
                                 </h4>
                                 <p class="text-md font-light mt-2 text-white">
-                                    The Arctic Ocean freezes every winter and much of the
-                                    sea-ice then thaws every summer, and that process will
-                                    continue whatever happens.
+                                    私たちの診断ツールは、ビジネスパーソンの皆様が客観的な視点から自己を見つける手助けをします。自己理解を深めることは、成功への道を切り開く重要な一歩です。ツールを使うことで、あなたのスキル、特性、価値観が明らかになり、自己ブランディングやキャリア計画の戦略立案に役立ちます。<br>
+
+                                    個々の強みと課題を客観的に把握することで、適切な成長戦略を見つけることができます。他者とのコミュニケーションが円滑になり、リーダーシップスキルが向上することで、ビジネスの成功に寄与します。ツールを活用して、今後の道を明確にし、自己成長の旅を効果的にサポートしましょう。
                                 </p>
                             </blockquote>
                         </div>
@@ -199,16 +206,17 @@
                     </div>
                     <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
                         <div class="md:pr-12">
-                            <div
+                            {{-- <div
                                 class="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300">
                                 <i class="fas fa-rocket text-xl"></i>
-                            </div>
-                            <h3 class="text-3xl font-semibold">A growing company</h3>
+                            </div> --}}
+                            <h3 class="text-3xl font-semibold">株式会社OnLine X 九州産業大学</h3>
                             <p class="mt-4 text-lg leading-relaxed text-gray-600">
-                                The extension comes with three pre-built pages to help you get
-                                started faster. You can change the text and images and you're
-                                good to go.
+                                <strong clss="text-bold">■アセスメントツールの詳細</strong><br>
+                            </p><br>
+                            ＜特徴＞ <br>
                             </p>
+
                             <ul class="list-none mt-6">
                                 <li class="py-2">
                                     <div class="flex items-center">
@@ -219,7 +227,7 @@
                                         </div>
                                         <div>
                                             <h4 class="text-gray-600">
-                                                Carefully crafted components
+                                                4つの分野、7項目にわけてビジネスパーソンのビジネス力を測定
                                             </h4>
                                         </div>
                                     </div>
@@ -232,7 +240,7 @@
                                                     class="fab fa-html5"></i></span>
                                         </div>
                                         <div>
-                                            <h4 class="text-gray-600">Amazing page examples</h4>
+                                            <h4 class="text-gray-600">自分の長所・短所を可視化</h4>
                                         </div>
                                     </div>
                                 </li>
@@ -244,11 +252,21 @@
                                                     class="far fa-paper-plane"></i></span>
                                         </div>
                                         <div>
-                                            <h4 class="text-gray-600">Dynamic components</h4>
+                                            <h4 class="text-gray-600">
+                                                当社と九州産業大学商学部聞間研究室により、ビジネス、経営での知見とを統計的に測定し、診断結果によって、当社からの目標達成に向かうアドバイス付き
+                                            </h4>
                                         </div>
                                     </div>
                                 </li>
                             </ul>
+                            <br>
+                            アセスメントツールのアドバイスは、九州産業大学商学部教授 聞間 理氏が担当、統計のアドバイスは、九州産業大学基礎教育センター講師 中世古 貴彦氏が担当。<br>
+
+                            <br> <strong class="text-bold">■株式会社OnLineについて</strong>
+                            <br>
+
+                            「リカレント教育をスタンダードにし、教育格差の解消に挑む」をミッションとし当社が運営するビジネススクールでは主にコーチング、セールスコミュニケーション、目標達成の技術を提供し、短期間でビジネス力をあげて、どこでも通用するビジネスパーソンを輩出します。講座内では、目標達成能力、コミュニケーション能力や問題解決能力などいわゆる『非認知能力』と言われる能力が総じて高まっていることを背景に、2022年には太宰府市立学業院中学校で「仕事」をテーマとした進路学習のサポートも実施している。
+
                         </div>
                     </div>
                 </div>
@@ -270,25 +288,23 @@
         <div class="container mx-auto px-4">
             <div class="flex flex-wrap">
                 <div class="w-full lg:w-6/12 px-4">
-                    <h4 class="text-3xl font-semibold">Let's keep in touch!</h4>
+                    {{-- <h4 class="text-3xl font-semibold">Let's keep in touch!</h4>
                     <h5 class="text-lg mt-0 mb-2 text-gray-700">
                         Find us on any of these platforms, we respond 1-2 business days.
-                    </h5>
+                    </h5> --}}
                     <div class="mt-6">
                         <button
                             class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                            type="button">
-                            <i class="flex fab fa-twitter"></i></button><button
+                            type="button"><a href="https://recurrent-edu.jp/"> <i
+                                    class="flex fa fa-home"></i></i></a></button>
+                        <button
                             class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                            type="button">
-                            <i class="flex fab fa-facebook-square"></i></button><button
+                            type="button"><a href="https://www.facebook.com/online.co.ltd">
+                                <i class="flex fab fa-facebook-square"></i></a></button>
+                        <button
                             class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                            type="button">
-                            <i class="flex fab fa-youtube"></i></button><button
-                            class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-                            type="button">
-                            <i class="flex fab fa-github"></i>
-                        </button>
+                            type="button"><a href="https://www.youtube.com/channel/UCpf1g6oUE_xySAcN4G1A_Xw">
+                                <i class="flex fab fa-youtube"></i></button>
                     </div>
                 </div>
                 <div class="w-full lg:w-6/12 px-4">
@@ -348,12 +364,14 @@
             </div>
         </div>
     </footer>
+
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <script>
+        function toggleNavbar(collapseID) {
+            document.getElementById(collapseID).classList.toggle("hidden");
+            document.getElementById(collapseID).classList.toggle("block");
+        }
+    </script>
 </body>
-<script>
-    function toggleNavbar(collapseID) {
-        document.getElementById(collapseID).classList.toggle("hidden");
-        document.getElementById(collapseID).classList.toggle("block");
-    }
-</script>
 
 </html>
