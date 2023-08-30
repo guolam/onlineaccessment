02e105ab5296c3654b4ebba4772c685c2f16640d
guolam <guolam123@gmail.com>
2023-08-30 13:04:25 +0900

    title

--- onlinetool/resources/views/layouts/guest.blade.php
+++ onlinetool/resources/views/layouts/guest.blade.php
@@ -6,10 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('/logo/logo.svg') }}" type="image/png">
-
-    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
-    <title>株式会社OnLine || ビジネス診断</title>
-
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
