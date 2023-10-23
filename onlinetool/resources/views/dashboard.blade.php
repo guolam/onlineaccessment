2649c54e81415fd0f25afdf7db26b0b1a3d60b33
guolam <guolam123@gmail.com>
2023-10-23 10:22:09 +0900

    dashboard

--- onlinetool/resources/views/dashboard.blade.php
+++ onlinetool/resources/views/dashboard.blade.php
@@ -7,6 +7,10 @@
 
     <body>
         @auth
+            <div class="text-center my-5">
+                1人様につき、2回まで診断できる仕組みになっております。<br>
+                それ以上超えると、回答できなくなりますので、診断のタイミングをご注意ください。
+            </div>
             <div class="flex justify-center">
                 <x-primary-button id="questionnaire" class="mt-4 mb-4 py-2 px-4 bg-blue-500 text-black rounded-lg">
                     <a href="{{ route('test') }}">新たな診断を受ける</a>
