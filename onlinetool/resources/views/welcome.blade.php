b89a0ee0ea2dfa39ebbf74a6b31fcc82a1118d21
guolam <guolam123@gmail.com>
2023-10-25 13:24:54 +0900

    favicon

--- onlinetool/resources/views/welcome.blade.php
+++ onlinetool/resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
-    <link rel="icon" src="{{ asset('img/logo/logo.jpg') }}" alt="株式会社OnLine" type="image/png">
-    <link rel="apple-touch-icon" sizes="100x100" href="./assets/img/apple-icon.png" />
+    <link rel="icon" href="{{ asset('img/logo/logo.jpg') }}" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet"
